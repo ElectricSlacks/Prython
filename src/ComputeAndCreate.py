248f817899e6cd875a93923628ffa17751224ba5
ElectricSlacks <35742210+ElectricSlacks@users.noreply.github.com>
2018-09-07 11:11:58 +0200

    Update ComputeAndCreate.py

--- src/ComputeAndCreate.py
+++ src/ComputeAndCreate.py
@@ -2,14 +2,14 @@ import math
 file = open('Primes.txt', 'w')
 
 
-for num in range(1,i):     #to iterate between 10 to infinity
+for num in range(1,30):     #to iterate between 10 to infinity
    for i in range(2,num):    #to iterate on the factors of the number
       if num%i == 0:         #to determine the first factor
          j=num/i             #to calculate the second factor
          
          break #to move to the next number, the #first FOR
       else:                  # else part of the loop
-         file.write(num)
+         file.write("str.num")
          file.write("\r\n")
 file.close()
 
