e3a6e3282ea2415bb5a4c6a8e8c91c70afa51d35
ElectricSlacks <35742210+ElectricSlacks@users.noreply.github.com>
2018-09-07 09:59:07 +0200

    Update and rename ComputeAndCreate.py to src/ComputeAndCreate.py

---
+++ src/ComputeAndCreate.py
@@ -0,0 +1,21 @@
+import math 
+def simpleSieve(sieveSize):
+    #creating Sieve.
+    sieve = [True] * (sieveSize+1)
+    # 0 and 1 are not considered prime.
+    sieve[0] = False
+    sieve[1] = False
+    for i in xrange(2,int(math.sqrt(sieveSize))+1):
+        if sieve[i] == False:
+            continue
+        for pointer in xrange(i**2, sieveSize+1, i):
+            sieve[pointer] = False
+    # Sieve is left with prime numbers == True
+    primes = []
+    for i in xrange(sieveSize+1):
+        if sieve[i] == True:
+            primes.append(i)
+    return primes
+
+sieveSize = input()
+primes = simpleSieve(sieveSize)
